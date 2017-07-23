c24026829f8f1d5b5a12c23d96f2fb9e5d981d02
chaitanyathengdi <thengdi.chaitanya.sudhir@gmail.com>
2017-07-24 01:01:15 +0530

    Add functionality to edit keys and fix reset logic

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ angular.module('jsonGenerator', [])
     vm.addKey = addKey;
     vm.saveFile = function(name){ fileService.saveFile(name, vm.generatedJson); };
     vm.loadFile = loadFile;
+    vm.checkValue = checkValue;
+    vm.resetJson = resetJson;
 
     function addKey(key, value) {
         vm.generatedJson[key] = value;
         vm.key=null;
         vm.value=null;
+    }
+    
+    function checkValue(key) {
+        if(!!key) {
+            if(!!vm.generatedJson[key]) {
+                vm.value = vm.generatedJson[key];
+            }
+        }
     }
 
     function loadFile() {
@@ -26,6 +36,12 @@ angular.module('jsonGenerator', [])
                     vm.generatedJson = data;
                 }, function() {});
         }
+    }
+    
+    function resetJson() {
+        vm.generatedJson = {
+            Title: "Sample title"
+        };
     }
 })
 .service('fileService', function($http, $filter){
